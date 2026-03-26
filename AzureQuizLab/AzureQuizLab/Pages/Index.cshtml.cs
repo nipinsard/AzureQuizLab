0f7111882a6ebc08e6ce8382d35c396689698c17
Nicolas PINSARD <n.pinsard.ext@fayatit.fayat.com>
2026-03-26 13:28:50 +0100

    Initial project

---
+++ AzureQuizLab/AzureQuizLab/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureQuizLab.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
