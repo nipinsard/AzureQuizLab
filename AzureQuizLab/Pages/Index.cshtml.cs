8a7495dfecbf2a5f723c90c99d532ac96f890e14
Nicolas PINSARD <n.pinsard.ext@fayatit.fayat.com>
2026-03-26 16:38:26 +0100

    Maintenance mode

--- AzureQuizLab/Pages/Index.cshtml.cs
+++ AzureQuizLab/Pages/Index.cshtml.cs
@@ -1,13 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AzureQuizLab.Pages
 {
     public class IndexModel : PageModel
     {
+        public bool MaintenanceMode { get; set; }
+
         public void OnGet()
         {
-
+            var configurationManager = new ConfigurationManager();
+            MaintenanceMode = configurationManager.GetValue<bool>("MaintenanceMode", false);
         }
     }
 }
