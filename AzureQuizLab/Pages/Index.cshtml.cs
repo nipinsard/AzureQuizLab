210fdf1e6495ca9041d74bc6c383b0bd40fab0aa
PINSARD Nicolas External <n.pinsard.ext@fayatit.fayat.com>
2026-06-04 13:53:57 +0200

    Change

--- AzureQuizLab/Pages/Index.cshtml.cs
+++ AzureQuizLab/Pages/Index.cshtml.cs
@@ -4,12 +4,14 @@ namespace AzureQuizLab.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
         private readonly IConfiguration _configuration;
 
         public bool MaintenanceMode { get; set; }
 
         public IndexModel(IConfiguration configuration)
         {
+            _logger = logger;
             _configuration = configuration;
         }
 
--- AzureQuizLab/Program.cs
+++ AzureQuizLab/Program.cs
@@ -23,4 +23,6 @@ app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 
+
+
 app.Run();
